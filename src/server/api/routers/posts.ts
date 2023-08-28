068ad33d5232081a3074956558b26c07a11a803c
LoganLeeBrown <logbrownlee@gmail.com>
2023-08-28 16:11:44 -0500

    Add username pre fetching to profile page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,20 +5,10 @@ import {
   publicProcedure,
 } from "(~/)/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profilePicture: user.imageUrl,
-    firstName: user.firstName,
-    lastName: user.lastName,
-  };
-};
+import { filterUserForClient } from "../../helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
