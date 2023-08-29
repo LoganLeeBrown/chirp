b7853d7eb3b333775392700aecf4bafab7d86f5a
LoganLeeBrown <logbrownlee@gmail.com>
2023-08-29 15:47:02 -0500

    Add borders to profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -22,17 +22,18 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div className="relative h-48 bg-slate-600">
+        <div className="relative h-36 bg-slate-600">
           <Image
             src={data.profilePicture}
             alt={`${data.username ?? ""}'s profile pic`}
             width={128}
             height={128}
-            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black"
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
           />
         </div>
         <div className="h-[64px]" />
-        <div className="p-4">{data.username}</div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400" />
       </PageLayout>
     </>
   );
