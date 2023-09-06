227bee39774c06472c696811bc3598cc9f5caa79
LoganLeeBrown <logbrownlee@gmail.com>
2023-09-05 23:44:55 -0500

    Add link to home page and portfolibo page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 import type { PropsWithChildren } from "react";
 import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineSelect } from "react-icons/ai";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen flex-col justify-center md:flex-row">
       <div>
-        <div className="flex items-center justify-center py-2 md:flex-col">
+        <div className="flex justify-around py-2 md:flex-col ">
+          <a href="./">
+            <AiOutlineHome className="h-10 w-10 md:mx-2 md:my-2"></AiOutlineHome>
+          </a>
           <Link href="https://portfolibo.vercel.app/">
-            <AiOutlineHome className="h-10 w-10"></AiOutlineHome>
+            <AiOutlineSelect className="h-10 w-10 md:mx-2 md:my-2"></AiOutlineSelect>
           </Link>
         </div>
       </div>
